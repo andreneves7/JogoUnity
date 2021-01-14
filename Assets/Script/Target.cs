c06be5b9c64b2635d8c8f62b12b8618b3c831004
jatsm <jatsmneves@gmail.com>
2021-01-14 02:28:36 +0000

    Ja tenho os wayPoints para deslocar os alvos mas vao todos para la nao so o desejado

--- Assets/Script/Target.cs
+++ Assets/Script/Target.cs
@@ -3,7 +3,27 @@
 public class Target : MonoBehaviour
 {
     public float healf = 50f;
-    
+   private Transform target;
+    private int waypointIndex = 0;
+    public float speed = 3f;
+
+    void Start()
+    {
+        target = WayPoint.waypoint[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+       //transform.LookAt(dir + transform.position);
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+        {
+            nextWayPoint();
+        }
+    }
+
+
     public void TakeDamage(float amout)
     {
         healf -= amout;
@@ -17,5 +37,23 @@ public class Target : MonoBehaviour
     {
         
         Destroy(gameObject);
+    }
+
+
+    void nextWayPoint()
+    {
+        //if (waypointIndex == 1)
+        //{
+        //    waypointIndex--;
+        //    target = WayPoint.waypoint[waypointIndex];
+        //}
+        //else if (waypointIndex == 0)
+        //{
+        //    waypointIndex++;
+        //    target = WayPoint.waypoint[waypointIndex];
+        //}
+        waypointIndex++;
+        target = WayPoint.waypoint[waypointIndex];
+
     }
 }
---
+++ Assets/Script/WayPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public static Transform[] waypoint;
+
+    void Awake()
+    {
+
+        waypoint = new Transform[transform.childCount];
+        for (int i = 0; i < waypoint.Length; i++)
+        {
+            waypoint[i] = transform.GetChild(i);
+        }
+        
+    }
+
+}
